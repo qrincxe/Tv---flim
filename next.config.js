0403c8a614718d4f84aee44e783d4d18168d9fba
qrincxe <qrincxe@gmail.com>
2025-01-05 15:38:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    TMDB_API_KEY: process.env.TMDB_API_KEY,
+    TMDB_API_KEY: process.env.adc48d20c0956934fb224de5c40bb85d,
   },
   images: {
     domains: ['image.tmdb.org'],
