e71652a0755cf3bceafcb87e8f06dcb6fdc26a09
qrincxe <qrincxe@gmail.com>
2025-01-11 10:12:20 +0530

    Update constants.ts

--- ultis/constants.ts
+++ ultis/constants.ts
@@ -8,11 +8,11 @@ export const mediaTypes: MediaType[] = ['all', 'movie', 'tv'];
 export const imageOriginal = (imgPath: string) =>
   `${TMDB_IMAGE}original${imgPath}`;
 export const w500Image = (imgPath: string) => `${TMDB_IMAGE}w500${imgPath}`;
-export const embedMovie = (id: number) => `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
+export const embedMovie = (id: number) => `https://flicky.host/embed/movie/?id=${id}`;
 export const imageResize = (src: string, dimension: string = 'w200') =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const embedEpisode = (id: number, season: number, episode: number) =>
-  `https://2embed.org/embed/series?tmdb=${id}&sea=${season}&epi=${episode}`;
+  `https://flicky.host/embed/tv?id=${id}/${season}/${episode}`;
 
 export const airDate = (firstAir: string, lastAir: string) => {
   return firstAir === lastAir ? firstAir : `${firstAir} - ${lastAir}`;
